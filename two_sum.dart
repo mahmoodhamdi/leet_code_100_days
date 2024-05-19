ec181a1e9c2f6b462fe1cec3f17282ec373c2910
Mahmoodhamdi <hmdy7486@gmail.com>
2024-05-19 11:24:35 +0300

    add some explains

--- two_sum.dart
+++ two_sum.dart
@@ -1,3 +1,17 @@
+// class Solution {
+//  List<int> twoSum(List<int> nums, int target) {
+// for (var i = 0; i < nums.length; i++) {
+//   for (var j = i + 1; j < nums.length; j++) {
+//     if (nums[i] + nums[j] == target) {
+//       return [i, j];
+//     }
+//   }
+// }
+// return [];
+//  }
+// }
+// complexity O(n^2) time and O(1) space
+//note: use two loops but the time complexity of the algorithm to O(n^2) and space complexity of O(1)
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
     Map<int, int> numMap =
@@ -21,3 +35,5 @@ class Solution {
     return []; // Return an empty list if no solution is found
   }
 }
+// complexity O(n) time and O(n) space
+//note: use hashmap to store the numbers and their indices to reduce the time complexity of the algorithm to O(n)
